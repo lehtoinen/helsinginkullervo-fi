67502ed669bebb4579b024535d1a449194893a7b
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2018-05-10 12:48:15 +0300

    Added check for window to enable local dev

--- src/state/store.js
+++ src/state/store.js
@@ -5,7 +5,9 @@ import reducer from './reducer';
 
 function loadDevTools() {
   /* eslint-disable */
-  return process.env.NODE_ENV === 'development' && window.devToolsExtension
+  return process.env.NODE_ENV === 'development' &&
+    typeof window !== 'undefined' &&
+    window.devToolsExtension
     ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
         window.__REDUX_DEVTOOLS_EXTENSION__()
     : f => f;
