879b37344070bd89ddd965094fc6b71df21deab3
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2018-05-10 11:41:45 +0300

    Added redux store for ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,17 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
+import { Provider } from 'react-redux';
+import { renderToString } from 'react-dom/server';
+import store from './src/state/store';
 
- // You can delete this file if you're not using it
+exports.replaceRenderer = ({
+  bodyComponent,
+  replaceBodyHTMLString,
+  setHeadComponents,
+}) => {
+  // Connect Redux store
+  const ConnectedBody = () => (
+    <Provider store={store}>{bodyComponent}</Provider>
+  );
+  const html = renderToString(<ConnectedBody />);
+  replaceBodyHTMLString(html);
+};
