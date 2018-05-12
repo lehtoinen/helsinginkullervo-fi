a9daedaee635fe95c49ed150ffd2b439fb8f0eaf
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2018-05-12 10:49:59 +0300

    Added redux-persist

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
-import store from './src/state/store';
+import { store, persistor } from './src/state/store';
 
 exports.replaceRouterComponent = ({ history }) => {
   const ConnectedRouterWrapper = ({ children }) => (
     <Provider store={store}>
-      <Router history={history}>{children}</Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router history={history}>{children}</Router>
+      </PersistGate>
     </Provider>
   );
 
