a1089d22f9b1f9fdd9031d8c3a3093921f7ca37f
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2018-05-13 14:22:14 +0300

    selected changed to optional, added checks for values

--- src/components/FixturesList/FixturesFilter.js
+++ src/components/FixturesList/FixturesFilter.js
@@ -5,26 +5,34 @@ import styles from './FixturesFilter.module.scss';
 
 const FixturesFilter = ({ options, selected, onChange }) => (
   <div className={styles.root}>
-    {options.map(option => (
-      <div key={option} className={styles.option}>
-        <label htmlFor={`filter_${option}`}>
-          <input
-            onChange={() => onChange(option)}
-            type="checkbox"
-            id={`filter_${option}`}
-            selected={selected.indexOf(option) > -1 ? 'selected' : null}
-          />
-          {option}
-        </label>
-      </div>
-    ))}
+    {options.map(option => {
+      const isSelected = selected && selected.includes(option);
+      return (
+        <div key={option} className={styles.option}>
+          <label htmlFor={`filter_${option}`}>
+            <input
+              onChange={() => onChange(option)}
+              type="checkbox"
+              id={`filter_${option}`}
+              selected={isSelected ? 'selected' : null}
+              defaultChecked={isSelected}
+            />
+            {option}
+          </label>
+        </div>
+      );
+    })}
   </div>
 );
 
 FixturesFilter.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
-  selected: PropTypes.arrayOf(PropTypes.string).isRequired,
   onChange: PropTypes.func.isRequired,
+  selected: PropTypes.arrayOf(PropTypes.string),
+};
+
+FixturesFilter.defaultProps = {
+  selected: [],
 };
 
 export default FixturesFilter;
