92d1cfd61f15224fb21d30f54d5d2ddf3c59b407
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2018-05-19 09:30:01 +0300

    Added manifest manually in favor of gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,18 +27,32 @@ module.exports = {
         test: /\.js$|\.jsx$/,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'Helsingin Kullervo',
-        short_name: 'Kultsi',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#d52c27',
-        display: 'minimal-ui',
-        icon: 'src/assets/img/kullervo_logo.png', // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: 'Helsingin Kullervo',
+    //     short_name: 'Kultsi',
+    //     start_url: '/',
+    //     background_color: '#ffffff',
+    //     theme_color: '#d52c27',
+    //     display: 'minimal-ui',
+    //     icon: 'src/assets/img/kullervo_logo.png',
+
+    //     logo: "./src/path/to/your/favicon",
+    //     injectHTML: true,
+    //     icons: {
+    //       android: true,
+    //       appleIcon: true,
+    //       appleStartup: true,
+    //       coast: false,
+    //       favicons: true,
+    //       firefox: true,
+    //       twitter: false,
+    //       yandex: false,
+    //       windows: false
+    //     }
+    //   },
+    // },
     'gatsby-plugin-netlify',
   ],
   proxy: {
