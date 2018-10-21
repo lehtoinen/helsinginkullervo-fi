1a166ee18f2f1cc8382372143c3afa16747e435f
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2018-10-21 16:53:27 +0300

    Disabled competition queries temporarily

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,60 +42,60 @@ module.exports = {
               competition_id: 'hkifs1819',
             },
           },
-          {
-            method: 'getGroup',
-            options: {
-              competition_id: 'hkijp18', // Helsinki jalkapallo 2018
-              category_id: 'M7', // Miehet Seiska
-              group_id: '1', // lohko 1
-              matches: '0', // ei ottelulistausta
-            },
-          },
-          {
-            method: 'getGroup',
-            options: {
-              competition_id: 'hkijp18', // Helsinki jalkapallo 2018
-              category_id: 'M6', // Miehet Kutonen
-              group_id: '2', // lohko 2
-              matches: '0', // ei ottelulistausta
-            },
-          },
-          {
-            method: 'getGroup',
-            options: {
-              competition_id: 'hkijp18', // Helsinki jalkapallo 2018
-              category_id: 'M35', // Miehet Kutonen
-              group_id: '6', // lohko syksy 3
-              matches: '0', // ei ottelulistausta
-            },
-          },
-          {
-            method: 'getGroup',
-            options: {
-              competition_id: 'hkijp18', // Helsinki jalkapallo 2018
-              category_id: 'M40', // Miehet Kutonen
-              group_id: '4', // lohko syksy 3
-              matches: '0', // ei ottelulistausta
-            },
-          },
-          {
-            method: 'getGroup',
-            options: {
-              competition_id: 'hkijp18', // Helsinki jalkapallo 2018
-              category_id: 'M50', // Miehet Kutonen
-              group_id: '4', // lohko syksy 2
-              matches: '0', // ei ottelulistausta
-            },
-          },
-          {
-            method: 'getGroup',
-            options: {
-              competition_id: 'hkijp18', // Helsinki jalkapallo 2018
-              category_id: 'M55', // Miehet Kutonen
-              group_id: '4', // lohko jatko 2
-              matches: '0', // ei ottelulistausta
-            },
-          },
+          // {
+          //   method: 'getGroup',
+          //   options: {
+          //     competition_id: 'hkijp18', // Helsinki jalkapallo 2018
+          //     category_id: 'M7', // Miehet Seiska
+          //     group_id: '1', // lohko 1
+          //     matches: '0', // ei ottelulistausta
+          //   },
+          // },
+          // {
+          //   method: 'getGroup',
+          //   options: {
+          //     competition_id: 'hkijp18', // Helsinki jalkapallo 2018
+          //     category_id: 'M6', // Miehet Kutonen
+          //     group_id: '2', // lohko 2
+          //     matches: '0', // ei ottelulistausta
+          //   },
+          // },
+          // {
+          //   method: 'getGroup',
+          //   options: {
+          //     competition_id: 'hkijp18', // Helsinki jalkapallo 2018
+          //     category_id: 'M35', // Miehet Kutonen
+          //     group_id: '6', // lohko syksy 3
+          //     matches: '0', // ei ottelulistausta
+          //   },
+          // },
+          // {
+          //   method: 'getGroup',
+          //   options: {
+          //     competition_id: 'hkijp18', // Helsinki jalkapallo 2018
+          //     category_id: 'M40', // Miehet Kutonen
+          //     group_id: '4', // lohko syksy 3
+          //     matches: '0', // ei ottelulistausta
+          //   },
+          // },
+          // {
+          //   method: 'getGroup',
+          //   options: {
+          //     competition_id: 'hkijp18', // Helsinki jalkapallo 2018
+          //     category_id: 'M50', // Miehet Kutonen
+          //     group_id: '4', // lohko syksy 2
+          //     matches: '0', // ei ottelulistausta
+          //   },
+          // },
+          // {
+          //   method: 'getGroup',
+          //   options: {
+          //     competition_id: 'hkijp18', // Helsinki jalkapallo 2018
+          //     category_id: 'M55', // Miehet Kutonen
+          //     group_id: '4', // lohko jatko 2
+          //     matches: '0', // ei ottelulistausta
+          //   },
+          // },
         ],
       },
     },
