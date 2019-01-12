03de362b741bd221cb31ae0f4f71ca6d7dfbe068
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2019-01-12 16:11:26 +0200

    Added Table component

---
+++ src/components/layout/Table.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Table.module.scss';
+
+const Table = ({ columnTitles, rows }) => (
+  <div className={styles.root}>
+    <div className={`${styles.tableRow} ${styles.tableHead}`}>
+      {columnTitles.map(title => title)}
+    </div>
+    {rows.map(row => (
+      <row.type
+        {...row.props}
+        {...{
+          className: `${styles.tableRow} ${
+            row.props.highlight ? styles.highlight : null
+          }`,
+        }}
+      />
+    ))}
+  </div>
+);
+// <div className={styles.tableRow}>{row}</div>)}
+
+Table.propTypes = {
+  columnTitles: PropTypes.arrayOf(PropTypes.node),
+  rows: PropTypes.arrayOf(PropTypes.node),
+};
+
+Table.defaultProps = {
+  columnTitles: [],
+  rows: [],
+};
+
+export default Table;
