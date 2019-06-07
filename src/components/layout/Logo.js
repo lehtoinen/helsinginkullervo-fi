bdf3d144edf5e4f4d5857dd1db1a6367f17e9eca
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2019-06-07 22:28:40 +0300

    Updated logo and base color

--- src/components/layout/Logo.js
+++ src/components/layout/Logo.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import logo from '../../assets/img/kullervo.svg';
+// import logo from '../../assets/img/kultsi_2019.svg';
 import styles from './Logo.module.scss';
 
-const Logo = () => <div className={styles.root} />;
+const Logo = () => (
+  <img src={logo} alt="Helsingin Kullervo logo" className={styles.root} />
+);
 
 export default Logo;
