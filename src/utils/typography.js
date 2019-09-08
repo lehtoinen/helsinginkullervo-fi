9a9c9ea3a4439792fc4af1fea105607c0886881a
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2019-09-08 18:54:13 +0300

    Updated typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,11 +1,33 @@
 import Typography from 'typography';
 import theme from 'typography-theme-kirkham';
 
+theme.overrideThemeStyles = ({ rhythm }, options) => ({
+  'h1,h2,h3': {
+    marginTop: rhythm(1 / 4),
+    marginBottom: rhythm(1 / 2),
+  },
+  h4: {
+    marginBottom: 0,
+  },
+});
+
 const typography = new Typography({
   ...theme,
   ...{
     includeNormalize: false,
     baseFontSize: '20px',
+    headerFontFamily: ['Nunito', 'sans-serif'],
+    bodyFontFamily: ['Lato', 'sans-serif'],
+    googleFonts: [
+      {
+        name: 'Nunito',
+        styles: ['400'],
+      },
+      {
+        name: 'Lato',
+        styles: ['400', '400i', '700', '700i', '900'],
+      },
+    ],
   },
 });
 
