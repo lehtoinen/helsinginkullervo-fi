bb1d465ed7d40d15cf949affb7c1975ad2986797
lehtoinen <lehtovaaralainen@gmail.com>
2020-05-29 18:32:36 +0300

    Updated torneopal configuration for the 2020 season

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,44 +61,80 @@ module.exports = {
           {
             method: 'getMatches',
             options: {
-              // season_id: '2018-19',
               club_id: 571,
-              competition_id: 'etefs1920',
+              competition_id: 'etejp20',
             },
           },
           {
             method: 'getGroup',
             injectData: {
+              title: 'Seiska alkusarja',
               title: 'Futsal harrastekakkonen, lohko 3',
             },
             options: {
-              competition_id: 'etefs1920', // Uusimaa futsal 2019-20
-              category_id: 'FMH2', // Miehet Futsal Harrastekakkonen
-              group_id: '3', // lohko 3
+              competition_id: 'etejp20', // Uusimaa jalkapallo 2019
+              category_id: 'M7', // Miehet Seiska
+              group_id: '4', // lohko 4
               matches: '0', // ei ottelulistausta
             },
           },
           {
             method: 'getGroup',
             injectData: {
-              title: 'Futsal harrastekakkonen, lohko 1',
+              title: 'Kutonen',
             },
             options: {
-              competition_id: 'etefs1920', // Uusimaa futsal 2019-20
-              category_id: 'FMH2', // Miehet Futsal Harrastekakkonen
-              group_id: '1', // lohko 1
+              competition_id: 'etejp20', // Uusimaa jalkapallo 2019
+              category_id: 'M6', // Miehet Kutonen
+              group_id: '4', // lohko 4
               matches: '0', // ei ottelulistausta
             },
           },
           {
             method: 'getGroup',
             injectData: {
-              title: 'Futsal Vitonen',
+              title: 'KKI-35',
             },
             options: {
-              competition_id: 'etefs1920', // Uusimaa futsal 2019-20
-              category_id: 'FM5', // Miehet Futsal Vitonen
-              group_id: '3', // lohko 3
+              competition_id: 'etejp20', // Uusimaa jalkapallo 2019
+              category_id: 'M35', // KKI-35
+              group_id: '4', // lohko 4
+              matches: '0', // ei ottelulistausta
+            },
+          },
+          {
+            method: 'getGroup',
+            injectData: {
+              title: 'KKI-40',
+            },
+            options: {
+              competition_id: 'etejp20', // Uusimaa jalkapallo 2019
+              category_id: 'M40', // KKI-40
+              group_id: '6', // lohko 6
+              matches: '0', // ei ottelulistausta
+            },
+          },
+          {
+            method: 'getGroup',
+            injectData: {
+              title: 'KKI-50',
+            },
+            options: {
+              competition_id: 'etejp20', // Uusimaa jalkapallo 2019
+              category_id: 'M50', // KKI-50
+              group_id: '5', // lohko 5
+              matches: '0', // ei ottelulistausta
+            },
+          },
+          {
+            method: 'getGroup',
+            injectData: {
+              title: 'KKI-55',
+            },
+            options: {
+              competition_id: 'etejp20', // Uusimaa jalkapallo 2019
+              category_id: 'M55', // KKI-55
+              group_id: '2', // lohko 2
               matches: '0', // ei ottelulistausta
             },
           },
